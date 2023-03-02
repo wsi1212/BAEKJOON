1c59e01553970ecacc71023b642915756a0da849
도현욱 <105199741+wsi1212@users.noreply.github.com>
2023-03-02 16:00:06 +0900

    P1000

---
+++ Java/P1000.java
@@ -0,0 +1,15 @@
+package Java;
+
+import java.util.Scanner;
+
+public class P1000 {
+
+    public static void main(String[] args) {
+      int a=0, b=0;
+        Scanner sc = new Scanner(System.in);
+        a = sc.nextInt();
+        b=sc.nextInt();
+        System.out.println(a+b);
+    }
+
+}
